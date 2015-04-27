5a346d660327a98926ffcb7dd2edb6bca9bed00b
Egan Anderson <p.egan.anderson@gmail.com>
2015-04-26 20:19:36 -0600

    added isBuzzed and isFizzed methods

--- FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
@@ -13,9 +13,48 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    // Insert code here
+    for(int i = 1; i<=100; i++){
+        BOOL *buzzed = isBuzzed(i);
+        BOOL *fizzed = isFizzed(i);
+        if(buzzed == true && fizzed == true){
+            NSLog(@"FizzBuzz");
+        } else if(buzzed == true && fizzed == false){
+            NSLog(@"Buzz");
+        } else if(buzzed == false && fizzed == true){
+            NSLog(@"Fizz");
+        } else {
+            NSLog(@"%i", i);
+        }
+        
+    }
+    
     
     return YES;
 }
+
+BOOL isBuzzed (int integer){
+    NSString *intString = [NSString stringWithFormat:@"%i", integer];
+    NSString *intThree = [NSString stringWithFormat:@"%i", 3];
+    if(integer % 3 == 0){
+        return true;
+    } else if([intString containsString:intThree]){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+BOOL isFizzed (int integer){
+    NSString *intString = [NSString stringWithFormat:@"%i", integer];
+    NSString *intFive = [NSString stringWithFormat:@"%i", 5];
+    if(integer % 5 == 0){
+        return true;
+    } else if([intString containsString:intFive]){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 @end
